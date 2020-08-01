91563e978b09caa056e23f88e217b125809fb063
rinsuki <428rinsuki+git@gmail.com>
2020-08-02 03:22:44 +0900

    first commit

---
+++ Package.swift
@@ -0,0 +1,25 @@
+// swift-tools-version:5.2
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "Ocean",
+    platforms: [
+        .macOS(.v10_15),
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/vapor/vapor", from: "4.27.1"),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .target(name: "App", dependencies: [
+            .product(name: "Vapor", package: "vapor"),
+        ]),
+        .target(name: "Run", dependencies: ["App"]),
+        .testTarget(name: "AppTests", dependencies: ["App"]),
+    ]
+)
---
+++ Sources/App/configure.swift
@@ -0,0 +1,5 @@
+import Vapor
+
+public func configure(_ app: Application) throws {
+    // currently nothing
+}
